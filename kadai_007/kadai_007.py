c850c0055a8fe07605171954160e2bab18b550cc
o1-mat <168581110+o1-mat@users.noreply.github.com>
2024-06-11 11:47:07 +0900

    Add files via upload

---
+++ kadai_007/kadai_007.py
@@ -0,0 +1,22 @@
+array = []
+array.append("月曜日は晴れです")
+array.append("火曜日は雨です")
+array.append("水曜日は晴れです")
+array.append("木曜日は晴れです")
+array.append("金曜日は曇りです")
+array.append("土曜日は曇りのち雨です")
+array.append("日曜日は雷雨です")
+print(array[2])
+
+
+dictionary = {}
+dictionary["mon"] = "晴れ"
+dictionary["tue"] = "雨"
+dictionary["wed"] = "晴れ"
+dictionary["thu"] = "晴れ"
+dictionary["fri"] = "曇り"
+dictionary["sat"] = "曇りのち雨"
+dictionary["sun"] = "雷雨"
+print(dictionary["wed"])
+
+
