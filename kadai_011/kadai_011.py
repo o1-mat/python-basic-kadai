7da4c104f6b285d28c3a1a91868e98de0df9ca2f
o1-mat <168581110+o1-mat@users.noreply.github.com>
2024-06-12 13:47:10 +0900

    Add files via upload

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,15 @@
+array = ["水","金","地","火","木","土","天","海","冥"]
+
+
+for value in array:
+    print(value)
+
+
+i = 0  
+while i < len(array):
+    value = array[i]
+    print(f"{value}")
+    i += 1
+    
+
+
